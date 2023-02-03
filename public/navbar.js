833a81ce9a02694d56914e9c69e69a1eb348028c
FilipDavid1 <filipenkodavid@gmail.com>
2023-02-03 19:39:24 +0100

    add cart to every page

--- public/navbar.js
+++ public/navbar.js
@@ -4,3 +4,19 @@ const hamburger = document.querySelector(".hamburger-menu");
             hamburger.classList.toggle("change");
             navbar.classList.toggle("show");
             });
+
+//display number of items in cart on window load
+window.onload = function() {
+    const updateCartCount = () => {
+        let cart = localStorage.getItem('cart');
+        if (cart === null) {
+          cart = [];
+        } else {
+          cart = JSON.parse(cart);
+        }
+        const cartCountElement = document.querySelector('.cart-count');
+        cartCountElement.textContent = cart.length;
+      };
+    
+      updateCartCount();
+}
